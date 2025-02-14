4142ae25c79c7566cca22d38661a1ba92083375b
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-14 18:07:45 +0100

    added functionality to theme change button

--- javascript.js
+++ javascript.js
@@ -13,6 +13,25 @@ if (!task || !date || !time)
 
 addTasks.addEventListener("click", () => addingTasks())
 
+//function for theme change
+let bgColor = document.querySelectorAll(".top, nav, .innerButtonThemeChange, circle, path")
+let addColor = document.querySelector(".addCircle")
+const themeChangeButton = document.querySelector(".themeChange")
+
+function themeChange(element) {
+    element.style.backgroundColor ="cyan"
+    element.style.stroke ="cyan"
+
+}
+
+themeChangeButton.addEventListener("click", () => {
+    addColor.style.fill = "cyan"
+    bgColor.forEach(element => {
+        themeChange(element);
+    })
+})
+
+
 //function task cleared
 let clear = document.querySelectorAll(".clear")
 
